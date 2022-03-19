84ec04a37d0ef709b79fd95746507ba6b7ce829f
Paul T. Maack <paultmaack@gmail.com>
2022-03-18 23:44:47 -0700

    Dunno how to resolve the error saying it's not a function. Oh well.

--- src/App.js
+++ src/App.js
@@ -5,50 +5,51 @@ import * as apiCalls from './api';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: [],
+        }
+
+        this.handleSave = this.handleSave.bind(this);
+        this.onDelete = this.onDelete.bind(this);
     }
 
-    this.handleSave = this.handleSave.bind(this);
-    this.onDelete = this.onDelete.bind(this);
-  }
+    componentDidMount() {
+        console.log("App this.props: ", this.props)
+        this.loadRecipes();
+    }
 
-  componentDidMount() {
-    console.log("App this.props: ", this.props)
-    this.loadRecipes();
-  }
+    async loadRecipes() {
+        const recipes = await apiCalls.getRecipes();
+        console.log("App loadRecipes: ", recipes);
+        this.setState({ recipes });
+    }
 
-  async loadRecipes() {
-    const recipes = await apiCalls.getRecipes();
-    console.log("App loadRecipes: ", recipes);
-    this.setState({ recipes });
-  }
+    async handleSave(recipe) {
+        console.log("App, handleSave : ", recipe)
+        const newRecipe = await apiCalls.createRecipe(recipe);
+        this.setState({ recipes: [...this.state.recipes, newRecipe] });
+    }
 
-  async handleSave(recipe) {
-    console.log("App, handleSave : ", recipe)
-    const newRecipe = await apiCalls.createRecipe(recipe);
-    this.setState({ recipes: [...this.state.recipes, newRecipe] });
-  }
- 
-  async onDelete(id) {
-    await apiCalls.removeRecipe(id);
-    const recipes = this.state.recipes.filter(r => r.id !== id);
-    this.setState({ recipes });
-  }
+    async onDelete(id) {
+        await apiCalls.removeRecipe(id);
+        const recipes = this.state.recipes.filter(r => r.id !== id);
+        this.setState({ recipes });
+    }
 
-  render() {
+    render() {
 
-    return (
-      <div className="App">
-        <Form
-          onSave={this.handleSave}
-        />
-        <List  recipes={this.state.recipes} />
-      </div>
-    );
-  }
+        return ( <
+            div className = "App" >
+            <
+            Form onSave = { this.handleSave }
+            /> <
+            List recipes = { this.state.recipes }
+            /> <
+            /div>
+        );
+    }
 }
 
 export default App;
