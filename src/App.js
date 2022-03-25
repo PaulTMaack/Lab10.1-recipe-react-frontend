b03c512e30e25301c2f965a15dd92b82774a2a1d
Paul T. Maack <paultmaack@gmail.com>
2022-03-24 21:36:56 -0700

    Forgot to format. Looks prettier now.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import './App.css';
 import React, { useEffect, useState } from 'react'
 
 
-function App(){
-     const [ recipesArray, setRecipesArray ] = useState([ ]);
+function App() {
+    const [recipesArray, setRecipesArray] = useState([]);
 
-     useEffect(()=> {
-     loadRecipes();
-     }, []);
-
-
-    const loadRecipes = async () => { 
+    useEffect(() => {
+        loadRecipes();
+    }, []);
+    const loadRecipes = async() => {
         const recipesArray = await apiCalls.getRecipes();
         console.log("App loadRecipes: ", recipesArray);
-        setRecipesArray( recipesArray); //[...recipesArray, newRecipe ]
+        setRecipesArray(recipesArray); //[...recipesArray, newRecipe ]
     }
 
-        const onDelete = async (id) => {
+    const onDelete = async(id) => {
         await apiCalls.removeRecipe(id);
         const recipesArray = recipesArray.filter(r => r.id !== id);
-        setRecipesArray( recipesArray);// [...recipesArray, newRecipe ]
+        setRecipesArray(recipesArray); // [...recipesArray, newRecipe ]
     }
 
     return (
@@ -38,53 +36,7 @@ function App(){
           }
      </div>
    );
+   
  }
 
-
-export default App;
-
-
-// class App extends Component {
-     //     componentDidMount() {
-     //         console.log("App this.props: ", this.props)
-     //         loadRecipes();
-     //     }
-     //     async handleSave(recipe) {
-     //         console.log("App, handleSave : ", recipe)
-     //         const newRecipe = await apiCalls.createRecipe(recipe);
-     //         this.setState({ recipes: [...this.state.recipes, newRecipe] });
-     //     }
-
-     //     async onDelete(id) {
-     //         await apiCalls.removeRecipe(id);
-     //         const recipes = this.state.recipes.filter(r => r.id !== id);
-     //         this.setState({ recipes });
-     //     }
-
-         //     constructor(props) {
-     //         super(props);
-     //         this.state = {
-     //             recipes: [],
-     //         }
-     /**
-      * Hook declaration, since it is about recipes and managing recipes state, 
-      * I will be using "recipeArray" and "setRecipesArray" in the array that returns from calling 
-      * useState(initialState), where initial state would be an empty array.
-      */
-     //         this.handleSave = this.handleSave.bind(this);
-     //         this.onDelete = this.onDelete.bind(this);
-     //     }
-
-     //     render() {
-
-     //         return ( <
-     //             div className = "App" >
-     //             <
-     //             Form onSave = { this.handleSave }
-     //             /> <
-     //             List recipes = { this.state.recipes }
-     //             /> <
-     //             /div>
-     //         );
-     //     }
-// }
+    export default App;
--- src/api.js
+++ src/api.js
@@ -3,40 +3,39 @@ const APIURL = '/api/recipes/';
 
 export async function getRecipes() {
 
-  return fetch(APIURL)
-  .then(resp => writeError())
-  }
+    return fetch(APIURL)
+        .then(resp => writeError())
+}
 
-  export async function createRecipe(recipe) {
+export async function createRecipe(recipe) {
     return fetch(APIURL, {
-      method: 'post',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-      body: JSON.stringify(recipe)
-    })
-    .then(resp => writeError())
-  }
-  
-  export async function removeRecipe(id) {
+            method: 'post',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify(recipe)
+        })
+        .then(resp => writeError())
+}
+
+export async function removeRecipe(id) {
     const deleteURL = APIURL + id;
     return fetch(deleteURL, {
-      method: 'delete'
-    })
-      .then(resp => writeError())
-  }
-  export async function writeError(props){
-     if (!props.ok) {
-          if (props.status >= 400 && props.status < 500) {
+            method: 'delete'
+        })
+        .then(resp => writeError())
+}
+export async function writeError(props) {
+    if (!props.ok) {
+        if (props.status >= 400 && props.status < 500) {
             return props.json().then(data => {
-              let err = { errorMessage: data.message };
-              throw err;
+                let err = { errorMessage: data.message };
+                throw err;
             })
-          } else {
+        } else {
             let err = { errorMessage: 'Please try again later, server is not responding' };
             throw err;
-          }
         }
-        return props.json();
-      }
-  
+    }
+    return props.json();
+}
